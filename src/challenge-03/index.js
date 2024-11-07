16ac6465134ca2f5b5402d43c2cb767853ac34ed
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-11-07 16:05:41 -0500

    refactor: Rename variables for better readability and merge loops

--- src/challenge-03/index.js
+++ src/challenge-03/index.js
@@ -3,27 +3,25 @@
  * @returns {number}
  */
 export function findSafestPath (dream) {
-  const n = dream.length
-  const m = dream[0].length
-  const costs = Array.from({ length: n }, () => new Array(m).fill(0))
+  const rows = dream.length
+  const columns = dream[0].length
+  const dangerLevels = Array.from({ length: rows }, () => new Array(columns).fill(0))
 
-  costs[0][0] = dream[0][0]
+  dangerLevels[0][0] = dream[0][0]
 
-  for (let i = 1; i < m; i++) {
-    costs[0][i] = costs[0][i - 1] + dream[0][i]
+  for (let i = 1; i < columns; i++) {
+    dangerLevels[0][i] = dangerLevels[0][i - 1] + dream[0][i]
   }
 
-  for (let j = 1; j < n; j++) {
-    costs[j][0] = costs[j - 1][0] + dream[j][0]
-  }
+  for (let i = 1; i < rows; i++) {
+    dangerLevels[i][0] = dangerLevels[i - 1][0] + dream[i][0]
 
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < m; j++) {
-      const fromTop = costs[i - 1][j]
-      const fromLeft = costs[i][j - 1]
-      costs[i][j] = Math.min(fromTop, fromLeft) + dream[i][j]
+    for (let j = 1; j < columns; j++) {
+      const fromTop = dangerLevels[i - 1][j]
+      const fromLeft = dangerLevels[i][j - 1]
+      dangerLevels[i][j] = Math.min(fromTop, fromLeft) + dream[i][j]
     }
   }
 
-  return costs[n - 1][m - 1]
+  return dangerLevels[rows - 1][columns - 1]
 }
